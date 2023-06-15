b8a02214a8cd3c3bb22dd9ec4468d616b4f60a64
wangddwei <1482466683@qq.com>
2023-06-15 17:32:40 +0800

    书城mybatis逆向工程测试数据库

--- book-store-dao/src/main/java/com/itwang/mapper/UserMapper.java
+++
@@ -1,8 +0,0 @@
-package com.itwang.mapper;
-
-import com.itwang.pojo.User;
-import org.apache.ibatis.annotations.Param;
-
-public interface UserMapper {
-    User userLoginByUsernameAndPassword(@Param("username") String username,@Param("password") String password);
-}
--- book-store-dao/src/test/java/com/itwang/mapper/UserMapperTest.java
+++ book-store-dao/src/test/java/com/itwang/mapper/UserMapperTest.java
@@ -1,34 +1,37 @@
 package com.itwang.mapper;
 
 import com.itwang.pojo.User;
-import com.itwang.utils.MD5Utils;
+import com.itwang.pojo.UserExample;
 import org.apache.ibatis.io.Resources;
 import org.apache.ibatis.session.SqlSession;
 import org.apache.ibatis.session.SqlSessionFactory;
 import org.apache.ibatis.session.SqlSessionFactoryBuilder;
 import org.junit.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 
 import java.io.IOException;
+import java.util.List;
 
 import static org.junit.Assert.*;
 
 public class UserMapperTest {
-
+    /**
+     * 测试查询user总数量
+     *
+     */
+//    @Autowired
+//    SqlSessionFactory sqlSessionFactory;
     @Test
-    public void userLoginByUsernameAndPassword() throws IOException {
-        SqlSessionFactory sessionFactory = new SqlSessionFactoryBuilder()
-                .build(Resources.getResourceAsStream("mybatis-config.xml"));
-        SqlSession sqlSession = sessionFactory.openSession();
-
+    public void countByExample() throws IOException {
+        SqlSessionFactory sqlSessionFactory = new SqlSessionFactoryBuilder().build(Resources.getResourceAsReader("mybatis-config.xml"));
+        SqlSession sqlSession = sqlSessionFactory.openSession();
         UserMapper userMapper = sqlSession.getMapper(UserMapper.class);
+        UserExample userExample = new UserExample();
+//        UserExample.Criteria criteria = userExample.createCriteria();
 
-        String username = "zhangsan123456";
-        String password = MD5Utils.getMD5Utils("123123123");
+        int rows = userMapper.countByExample(userExample);
 
-        User user = userMapper.userLoginByUsernameAndPassword(username, password);
+        System.out.println("rows = " + rows);
 
-        System.out.println("user = " + user);
-
-        sqlSession.close();
     }
 }
